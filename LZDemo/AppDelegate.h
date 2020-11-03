7262b8be87daee6bbc080bf77612f5b7945123ac
apple <153444073@qq.com>
2020-11-03 10:40:11 +0800

    init project

---
+++ LZDemo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  LZDemo
+//
+//  Created by apple on 2020/11/3.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ LZDemo/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  LZDemo
+//
+//  Created by apple on 2020/11/3.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
